68a8802312e7cd4d53216fa27c529c8b5e95dcfb
Bibek <bibek.roman1@gmail.com>
2023-08-16 11:08:30 +0530

    FilterBy(Sort,Order,Category,Brand) part implemented

---
+++ src/features/product-list/productAPI.js
@@ -0,0 +1,27 @@
+export function fetchAllProducts(){
+    return new Promise(async (resolve)=>{
+        const response=await fetch("http://localhost:8080/products");
+        const data=await response.json()
+        resolve({data});
+    })
+}
+
+export function fetchProductsByFilters(filter){
+
+    //filter={"category" : "smartphone"}
+    //sort={"_sort" : "price"}
+    //order={"_order" : "asc/desc"}
+
+    let queryString=''
+
+    for(let key in filter){
+        queryString += `${key}=${filter[key]}&`
+    }
+    return new Promise(async (resolve)=>{
+        const response=await fetch("http://localhost:8080/products?" + queryString)
+        const data=await response.json()
+        resolve({data})
+    })
+}
+
+
