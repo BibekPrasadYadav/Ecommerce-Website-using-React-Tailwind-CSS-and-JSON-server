5c32dacfe016ec720981d6280a60c1b9681a259d
Bibek <bibek.roman1@gmail.com>
2023-08-16 16:02:59 +0530

    Filter and Sorting Code Part is modified

--- src/features/product-list/components/ProductList.js
+++ src/features/product-list/components/ProductList.js
@@ -207,23 +207,30 @@ export default function ProductList() {
   const dispatch = useDispatch();
 
   const [filter, setFilter] = useState({});
+  const [sort, setSort] = useState({});
 
   const handleFilters = (e, section, option) => {
-    const newFilter = { ...filter, [section.id]: option.value };
+    console.log(e.target.checked);
+    const newFilter = { ...filter };
+    if (e.target.checked) {
+      if (newFilter[section.id]) newFilter[section.id].push(option.value);
+      else newFilter[section.id] = [option.value];
+    } else {
+      const index = newFilter[section.id].findIndex((el) => el===option.value);
+      newFilter[section.id].splice(index, 1);
+    }
+    console.log({ newFilter });
     setFilter(newFilter);
-    dispatch(fetchProductsByFiltersAsync(newFilter));
-    console.log(section.id, option.value);
   };
 
   const handleSort = (e, option) => {
-    const newFilter = { ...filter, _sort: option.sort, _order: option.order };
-    setFilter(newFilter);
-    dispatch(fetchProductsByFiltersAsync(newFilter));
+    const newSort = { sort: option.sort, _order: option.order };
+    setSort(newSort);
   };
 
   useEffect(() => {
-    dispatch(fetchAllProductsAsync());
-  }, [dispatch]);
+    dispatch(fetchProductsByFiltersAsync({ filter, sort }));
+  }, [dispatch, filter]);
 
   return (
     <div>
